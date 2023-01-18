b4d8a68c3308187df42a0f7ab6823b25f772cb27
ranoob <whyfck@gmail.com>
2023-01-18 11:59:34 -0300

    new: server/web/mobile setups

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+import { Habit } from '@/components'
+
+function App() {
+  return (
+    <>
+      <Habit />
+    </>
+  )
+}
+
+export default App
---
+++ web/src/components/index.tsx
@@ -0,0 +1,1 @@
+export { Habit } from './Habit'
