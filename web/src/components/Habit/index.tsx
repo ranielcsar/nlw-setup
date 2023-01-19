98c46cdeef552b2c51d49787206f12c28a4c8cf9
ranoob <whyfck@gmail.com>
2023-01-19 17:03:59 -0300

    rename: Habit to HabitDay

--- web/src/components/Habit/index.tsx
+++
@@ -1,9 +0,0 @@
-export type HabiProps = {}
-
-export function Habit() {
-  return (
-    <div className="bg-zinc-900 text-zinc-100 w-20 h-20 rounded flex items-center justify-center m-5">
-      Habit
-    </div>
-  )
-}
